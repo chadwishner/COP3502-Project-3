3a5724bbeaea6cb025df1f2c1e1377c15ca3cff3
chadwishner <chadwishner@gmail.com>
2017-03-25 00:26:11 -0400

    Add files via upload

---
+++ LucyTattooParlor.java
@@ -0,0 +1,32 @@
+
+public class LucyTattooParlor {
+	/**
+	* Computes how many minutes of work the specified tattoo artist has. * @param The array of customers for one particular tattoo artist
+	*/
+	public static int computeMinutesOfWork(TattooCustomer [] a) {
+		//TODO
+		return 0; 
+	}
+	
+	/**
+	* Adds customer to the waitlist for a specific artist.
+	* If the artist is at capacity (in terms of number of customers or minutes)
+	* Then the customer is not added and the method returns false
+	* If the customer is successfully added the method returns true
+	* @param
+	*/
+	public static boolean addCustomer(TattooCustomer [][] a, TattooCustomer c, int artistNum) {
+		//TODO 
+		return false;
+	}
+	
+	/**
+	* Adds customer to the shortest waitlist in terms of minutes. If some artists have equal length waitlists
+	* then the customer is added to the lowest numbered artist. If no artist has space then the method does not * add the customer, and returns false.
+	* TODO - finish this javadoc  
+	* @return true if the customer was added to the waitlist, false otherwise (if all artists were full) */
+	public static boolean addCustomer(TattooCustomer [][] a, TattooCustomer c) {
+	//TODO 
+	return false;
+	}
+}
