aa0493af04e46c66719a7ba2c84aba56487c8e59
chadwishner <chadwishner@gmail.com>
2017-03-27 15:54:37 -0400

    Finished

--- LucyTattooParlor.java
+++ LucyTattooParlor.java
@@ -1,11 +1,56 @@
+import java.util.*;
 
 public class LucyTattooParlor {
+
+	public static int maxCustomers;
+	
+	public static void main(String [] args){
+		
+		Scanner input = new Scanner(System.in);
+		
+		maxCustomers = Integer.parseInt(args[2]);
+		
+
+		System.out.println("Please enter your name.");
+		String name = input.next();
+			if (name.compareToIgnoreCase("Print Waitlist") == 0){
+				//print the waitlist array
+			}
+					
+		System.out.println("Hi " + name + ", what tatoo would you like?");
+		String tattoo = input.next();
+		
+		System.out.println("If you would like a specific tattoo artist, please enter their index (0, 1, 2, ect.). If you would like to be added to the shortest waiting list, please enter 'shortest'.");
+		String artist = input.next();
+					
+			if (artist.compareToIgnoreCase("shortest") == 0){
+				//add to shortest wait time
+			}
+			else {
+				// add the person to the specific artists index
+				
+			}
+		System.out.println("How many minutes is your tattoo expected to take?");
+		int time = input.nextInt();
+		 
+			
+//		}
+		//add if the artist's indexes are full or if time > 8 hours, then the customer cannot be added to that artist and must print out an error message.
+		//add if for if all artists are full in either indexes or in time (8 hour max)
+
+		//must add name, tattoo, and time to specified artist or artist of shortest time
+	}
+	
 	/**
-	* Computes how many minutes of work the specified tattoo artist has. * @param The array of customers for one particular tattoo artist
+	* Computes how many minutes of work the specified tattoo artist has. 
+	* @param The array of customers for one particular tattoo artist
 	*/
 	public static int computeMinutesOfWork(TattooCustomer [] a) {
-		//TODO
-		return 0; 
+		int total = 0;
+		for (int i = 0; i >= a.length; i++){
+			total = total + a[i].getMinutes(); 	
+		}		
+		return total; 
 	}
 	
 	/**
@@ -16,7 +61,16 @@ public class LucyTattooParlor {
 	* @param
 	*/
 	public static boolean addCustomer(TattooCustomer [][] a, TattooCustomer c, int artistNum) {
-		//TODO 
+		
+		if ((computeMinutesOfWork(a [artistNum]) <= 480) && (a[artistNum].length < maxCustomers)){
+			for (int i = 0; i < a[artistNum].length; i++){
+				if (a[artistNum][i] == null){
+					a[artistNum][i] = c;
+					return true;
+				}
+			}
+		}
+		
 		return false;
 	}
 	
@@ -24,11 +78,30 @@ public class LucyTattooParlor {
 	* Adds customer to the shortest waitlist in terms of minutes. If some artists have equal length waitlists
 	* then the customer is added to the lowest numbered artist. If no artist has space then the method does not * add the customer, and returns false.
 	* TODO - finish this javadoc  
-	* @return true if the customer was added to the waitlist, false otherwise (if all artists were full) */
+	* @return true if the customer was added to the waitlist, false otherwise (if all artists were full) 
+	*/
 	public static boolean addCustomer(TattooCustomer [][] a, TattooCustomer c) {
-	//TODO 
+	
+		int currentShortest = 0;
+		int filledSlots = 0;
+		
+		for (int i = 0; i< a[0].length; i++){
+			if(a[0][i] != null){
+				currentShortest++;
+			}
+		}
+					
+		for (int x = 0; x >= a.length; x++){
+			for (int j = 0; j < (a.length); j++){
+				for (int i = 0; i< a[j].length; i++){
+					if(a[j][i] != null){
+						filledSlots++;
+					}
+					if(filledSlots < current)
+				}
+			}
+		}	
 	return false;
 	}
-	
-	//testing github 6
+
 }
--- TattooCustomer.java
+++ TattooCustomer.java
@@ -1,38 +1,25 @@
 import java.util.Scanner;
 
 public class TattooCustomer {
-	public static void main(String [] args){
-		
-		Scanner input = new Scanner(System.in);
-		
+	
+	private String name;
+	private String tattoo;
+	private int minutes;
 
-		
-		System.out.println("Please enter your name.");
-		String name = input.next();
-			if (name.compareToIgnoreCase("Print Waitlist") == 0){
-				//print the waitlist array
-			}
-					
-		System.out.println("Hi " + name + ", what tatoo would you like?");
-		String tattoo = input.next();
-		
-		System.out.println("If you would like a specific tattoo artist, please enter their index (0, 1, 2, ect.). If you would like to be added to the shortest waiting list, please enter 'shortest'.");
-		String artist = input.next();
-					
-			if (artist.compareToIgnoreCase("shortest") == 0){
-				//add to shortest wait time
-			}
-			else {
-				// add the person to the specific artists index
-				
-			}
-		System.out.println("How many minutes is your tattoo expected to take?");
-		int time = input.nextInt();
-		 
-		
-		//add if the artist's indexes are full or if time > 8 hours, then the customer cannot be added to that artist and must print out an error message.
-		//add if for if all artists are full in either indexes or in time (8 hour max)
-
-		//must add name, tattoo, and time to specified artist or artist of shortest time
+	TattooCustomer (String name, String tattoo, int minutes){
+	
+		this.name = name;
+		this.tattoo = tattoo;
+		this.minutes = minutes;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	public String getTattoo() {
+		return this.tattoo;
+	}
+	public int getMinutes() {
+		return this.minutes;
 	}
 }
