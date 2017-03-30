e90f489f4857fc48412903c4a87e376713e119d8
chadwishner <chadwishner@gmail.com>
2017-03-30 01:46:16 -0400

    Finished

--- TattooCustomer.java
+++ TattooCustomer.java
@@ -1,22 +1,38 @@
 public class TattooCustomer {
 	
+	//create the private variables to store the characteristics of the object TattooCustomer
 	private String name;
 	private String tattoo;
 	private int minutes;
 
 	TattooCustomer (String name, String tattoo, int minutes){
 	
+		//map the constructors to the private variables
 		this.name = name;
 		this.tattoo = tattoo;
 		this.minutes = minutes;
 	}
 	
+	/**
+	 * Getter Method in order to retrieve TattooCustomer name
+	 * @return Name
+	 */
 	public String getName() {
 		return this.name;
 	}
+	
+	/**
+	 * Getter Method in order to retrieve TattooCustomer name
+	 * @return Tattoo
+	 */
 	public String getTattoo() {
 		return this.tattoo;
 	}
+	
+	/**
+	 * Getter method in order to retrieve TattooCustomer name
+	 * @return Minutes
+	 */
 	public int getMinutes() {
 		return this.minutes;
 	}
